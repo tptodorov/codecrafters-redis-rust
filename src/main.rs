2aa68afa1b02b6bda88212a5a4d5a4152e64d6a8
Todor Todorov <98095+tptodorov@users.noreply.github.com>
2024-05-01 14:25:07 +0200

    pass 1st stage

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::net::TcpListener;
 
 fn main() {
 
+    println!("Logs from your program will appear here!");
+
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
