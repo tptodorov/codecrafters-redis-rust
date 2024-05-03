f9101df46292b130917eda050c870db4fa84a635
Todor Todorov <98095+tptodorov@users.noreply.github.com>
2024-05-03 14:27:02 +0200

    set and get ops and storage

--- src/commands.rs
+++ src/commands.rs
@@ -1,17 +1,58 @@
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+
 use anyhow::anyhow;
+
 use crate::resp::RESP;
 
-pub fn command_handler(cmd: &str, params: &[RESP]) -> anyhow::Result<RESP> {
-    match cmd {
-        "PING" => Ok(RESP::String("PONG".to_string())),
-        "ECHO" => {
-            let param1 = params.get(0).unwrap_or(&RESP::Null);
+#[derive(Clone)]
+pub struct RedisServer {
+    store: Arc<RwLock<HashMap<String, String>>>,
+}
 
-            match param1 {
-                RESP::Bulk(param1) => Ok(RESP::Bulk(param1.to_owned())),
-                _ => Err(anyhow!("invalid echo  command {:?}", params)),
-            }
+impl RedisServer {
+    pub fn new() -> Self {
+        RedisServer {
+            store: Arc::new(RwLock::new(HashMap::new())),
         }
-        _ => Err(anyhow!("Unknown command {}", cmd)),
+    }
+
+
+    pub fn command_handler(&self, cmd: &str, params: &[RESP]) -> anyhow::Result<RESP> {
+        match cmd {
+            "PING" => Ok(RESP::String("PONG".to_string())),
+            "ECHO" => {
+                let param1 = params.get(0).unwrap_or(&RESP::Null);
+                match param1 {
+                    RESP::Bulk(param1) => Ok(RESP::Bulk(param1.to_owned())),
+                    _ => Err(anyhow!("invalid echo  command {:?}", params)),
+                }
+            }
+            "SET" => {
+                // minimal implementation of https://redis.io/docs/latest/commands/set/
+                // SET key value
+                match params {
+                    [RESP::Bulk(key), RESP::Bulk(value)] => {
+                        self.store.write().unwrap().insert(key.clone(), value.clone());
+                        Ok(RESP::String("OK".to_string()))
+                    }
+                    _ => Err(anyhow!("invalid set command {:?}", params)),
+                }
+            }
+            "GET" => {
+                // minimal implementation of https://redis.io/docs/latest/commands/get/
+                // GET key
+                match params {
+                    [RESP::Bulk(key)] => {
+                        match  self.store.read().unwrap().get(key)  {
+                            Some(value) => Ok(RESP::Bulk(value.clone())),
+                            None => Ok(RESP::Null),
+                        }
+                    }
+                    _ => Err(anyhow!("invalid set command {:?}", params)),
+                }
+            }
+            _ => Err(anyhow!("Unknown command {}", cmd)),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use std::io::{BufRead, Read, Write};
 use std::net::TcpListener;
 use std::thread;
 
-use anyhow::Result;
 use anyhow::bail;
+use anyhow::Result;
 
 use resp::{RESP, RESPReader};
+
+use crate::commands::RedisServer;
 
 mod resp;
 mod commands;
 
 fn main() {
-    println!("Logs from your program will appear here!");
+    println!("starting redis server");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    let server = RedisServer::new();
+
     for stream in listener.incoming() {
+        let server = server.clone(); // cheap op since server contains mostly references
         thread::spawn(move || {
             match stream {
                 Ok(_stream) => {
@@ -24,12 +28,13 @@ fn main() {
                     loop {
                         if let Some(command) = reader.next() {
                             println!("sending response to {:?}", command);
-                            if let Ok(response) = handler(&command) {
+                            if let Ok(response) = handler(&server, &command) {
                                 println!("{:?} -> {:?}", command, response);
                                 if let Err(err) = reader.response(&response) {
                                     println!("error while writing response: {}. terminating connection", err);
                                     break;
                                 }
+                                // loop continues to read the next message
                             } else {
                                 break;
                             }
@@ -46,12 +51,12 @@ fn main() {
     }
 }
 
-fn handler(message: &RESP) -> Result<RESP> {
+fn handler(server: &RedisServer, message: &RESP) -> Result<RESP> {
     println!("message: {:?}", message);
     match message {
         RESP::Array(array) =>
             match &array[..] {
-                [RESP::Bulk(command), params @ .. ] => commands::command_handler(command.to_uppercase().as_str(), params),
+                [RESP::Bulk(command), params @ .. ] => server.command_handler(command.to_uppercase().as_str(), params),
                 _ => bail!("Invalid message".to_string()),
             }
         ,
